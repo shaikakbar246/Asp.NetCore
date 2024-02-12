30614d3189166354918aa8c86c109949fed2fbf2
shaikakbar246 <shaikakbar246@gmail.com>
2024-02-12 16:54:17 +0530

    Index View  added

---
+++ CrudExample/Controllers/PersonsController.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CrudExample.Controllers
+{
+    public class PersonsController : Controller
+    {
+        [Route("persons/index")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
